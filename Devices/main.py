258f48c5adfb642ec8020b72e87994aa56e9a772
Elias Grünewald <info@eliasgruenewald.de>
2022-04-20 13:59:43 +0200

    Update main.py

--- Devices/main.py
+++ Devices/main.py
@@ -15,11 +15,10 @@ def start_Device(deviceName,accountNr,blockchain_connection,config_file):
 
 
 if __name__ == '__main__':
-    config_file = read_yaml("/home/nikolas/MEGA/Workplace/Informatik/Masterarbeit/Implementation/PythonProject/MasterThesis_SoftwareEngineering/CONFIG.yaml")
+    config_file = read_yaml("./CONFIG.yaml")
     blockchain_connection=BlockChainConnection(config_file=config_file)
     blockchain_connection.connect()
     for i in range(config_file["DEFAULT"]["NumberOfParticipants"]):
         thread=threading.Thread(target= start_Device,args=["Device_"+str(i+1),i,blockchain_connection,config_file])
         thread.start()
         time.sleep(1)
-
